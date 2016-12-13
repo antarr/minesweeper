7c3802e7bad0f4b8b899311be7d3beabd8b94c17
Antarr Byrd <antarr.byrd@gmail.com>
2016-12-13 12:38:46 -0800

    Add Block class and made changes to code to utilize new block class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
-describe Game do
+describe Minesweeper::Game do
+  let(:game) { described_class.new(10, 10) }
   it { should respond_to(:won?) }
   it { should respond_to(:grid) }
+  it { should respond_to(:choose) }
+  it { should respond_to(:ended?) }
+
 end
